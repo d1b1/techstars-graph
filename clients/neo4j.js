bad0d879fa2e389977b0ee6e401d51a33760b7e6
Stephan L. Smith <stephan@meenta.io>
2023-08-04 12:16:30 -0400

    Added in the variables for process

--- clients/neo4j.js
+++ clients/neo4j.js
@@ -1,8 +1,8 @@
 const neo4j = require('neo4j-driver');
 
-const uri = '';
-const user = '';
-const password = '';
+const uri = process.env.NEO4J_URI;
+const user = process.env.NEO4J_USER;
+const password = process.env.NEO4J_PASSWORD;
 
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
 
